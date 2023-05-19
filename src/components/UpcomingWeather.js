8d5a60bd0e91d884952a3afba38929ff25c1638f
EdASalazar <edasalazar123@gmail.com>
2023-05-18 18:41:03 -0700

    Building a list if Upcoming weather

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, Text } from "react-native";
+import { StyleSheet, SafeAreaView, Text, FlatList } from "react-native";
+
+
+const DATA = [
+  {},
+  {},
+  {}
+]
 
 const UpcomingWeather = () => {
 
   return (
     <SafeAreaView style={styles.container}>
       <Text>Upcoming Weather</Text>
+      <FlatList
+        data={}
+        renderItem={}
+      />
     </SafeAreaView>
   )
 }
