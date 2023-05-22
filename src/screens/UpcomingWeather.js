c1ec48154a58053ee882b90acf841d3d83e995d8
EdASalazar <edasalazar123@gmail.com>
2023-05-22 14:59:06 -0700

    Adding list view.

--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, Text, FlatList, View, StatusBar, ImageBackground } from "react-native";
-import { Feather } from '@expo/vector-icons'
+import { StyleSheet, SafeAreaView, Text, FlatList, StatusBar, ImageBackground } from "react-native";
+import ListItem from "../components/ListItem";
 
 
 const DATA = [
@@ -42,32 +42,23 @@ const DATA = [
   }
 ]
 
-const Item = (props) => {
-  const { dt_txt, min, max, condition } = props
-  return (
-    <View style={styles.item}>
-      <Feather name={'sun'} size={50} color={'white'} />
-      <Text style={styles.date}>{dt_txt}</Text>
-      <Text style={styles.temp}>{min}</Text>
-      <Text style={styles.temp}>{max}</Text>
-    </View>
-  )
-}
 
 const UpcomingWeather = () => {
   const renderItem = ({ item }) => (
-    <Item
+    <ListItem
       condition={item.weather[0].main}
       dt_txt={item.dt_txt}
       min={item.main.temp_min}
       max={item.main.temp_max}
     />
   )
+
+  const { container, image } = styles
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={container}>
       <ImageBackground 
         source={require('../../assets/beautiful-nature.jpg')} 
-        style={styles.image}>
+        style={image}>
         <Text>Upcoming Weather</Text>
         <FlatList
           data={DATA}
@@ -84,26 +75,6 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
     backgroundColor: 'royalblue',
-  },
-  item: {
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    flexDirection: 'row',
-    alignItems: 'center',
-    borderWidth: 5,
-    backgroundColor: 'pink',
-   
-  },
-  temp: {
-    color: 'white',
-    fontSize: 20,
-    padding: 10,
-  },
-  date: {
-    color: 'white',
-    fontSize: 50,
-    paddingLeft: 5,
   },
   image: {
     flex: 1,
