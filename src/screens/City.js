21697b3bbc6a05cddf47a694a09349a5aaa55ba3
EdASalazar <edasalazar123@gmail.com>
2023-05-22 15:30:58 -0700

    EOD Save

---
+++ src/screens/City.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { SafeAreaView, Text, StyleSheet } from 'react-native'
+
+const City = () => {
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>City</Text>
+
+    </SafeAreaView>
+  )
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+})
+
+export default City
